b599b6ec1658c05333000310cda2518135eb06d4
Daniil <67960469+BluetoothWiFi@users.noreply.github.com>
2024-05-11 14:53:25 +0300

    Update CheatCleaner.py

--- collapse/utils/CheatCleaner.py
+++ collapse/utils/CheatCleaner.py
@@ -1,5 +1,6 @@
 from .Logger import logger
 from .Selector import selector
+from .Settings import settings
 import shutil
 import os
 
@@ -18,13 +19,16 @@ class CheatCleaner:
             'C:\\Nursultan',
             f'C:\\Users\\{os.getlogin()}\\.avalon'
         ]
-
+    
     def scan_folders(self):
         if selector.ask('Remove all cheats folder [y,n]?'):
             for folder in self.folders:
                 if os.path.isdir(folder):
                     logger.info('Removing folder: ' + folder)
 
-                    shutil.rmtree(folder, ignore_errors=True)
+                    SafeDir = settings.get('SafeDirName')
+                    
+                    shutil.move(folder, SafeDir)
+                    shutil.rmtree(SafeDir, ignore_errors=True)
 
 cheatcleaner = CheatCleaner()
