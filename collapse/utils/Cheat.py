0bf3b994dd8f0fd7d3a6573766ce213729515579
dest4590 <80628386+dest4590@users.noreply.github.com>
2024-05-23 23:45:01 +0300

    Optimized libraries and natives for 1.12.2 cheats

--- collapse/utils/Cheat.py
+++ collapse/utils/Cheat.py
@@ -48,8 +48,17 @@ class Cheat:
 
         # Downloading requirements
         data.download('jre-21.0.2.zip')
-        data.download('libraries.zip')
-        data.download('natives.zip')
+        
+        if self.version.startswith('1.12'):
+            logger.debug('Downloading 1.12.2 libraries & natives')
+            data.download('libraries-1.12.zip')
+            data.download('natives-1.12.zip')
+            
+        else:
+            logger.debug('Downloading 1.12.2+ libraries & natives')
+            data.download('libraries.zip')
+            data.download('natives.zip')
+            
         data.download('assets.zip')
         
         logger.info(f'Running client {self.name}')
@@ -57,18 +66,46 @@ class Cheat:
             # Using backslash var, because f-strings not supporting it in expressions
             bc = '\\'
 
-            command = f"..\\jre-21.0.2\\bin\\java{'w' if self.silent else ''}.exe -Xverify:none -Xmx{settings.get('ram')}M -Djava.library.path={f'.{bc}natives;' if self.internal and os.path.isdir(f'natives') else f'..{bc}natives;'} {(f'-cp .{bc}libraries{bc}*' if self.internal and os.path.isdir(f'libraries') else f'-cp ..{bc}libraries{bc}*') + ';'}{f'.{bc}' + self.jar + f' {self.main_class}'} --username {settings.get('nickname')} --gameDir .\\ --assetsDir {f'.{bc}assets' if self.internal and os.path.isdir(f'assets') else f'..{bc}assets'} --assetIndex {self.version} --uuid N/A --accessToken 0 --userType legacy --version {self.version}"
+            path_sep = ';' if os.name == 'nt' else ':'
 
+            if self.internal and os.path.isdir('libraries'):
+                classpath = f'.{bc}libraries-1.12{bc}*' if self.version.startswith('1.12') else f'.{bc}libraries{bc}*'       
+            else:
+                classpath = f'..{bc}libraries-1.12{bc}*' if self.version.startswith('1.12') else f'..{bc}libraries{bc}*'
+
+            if self.internal and os.path.isdir('natives'):
+                native_path = f'.{bc}natives-1.12;' if self.version.startswith('1.12') else f'.{bc}natives;'
+            else:
+                native_path = f'..{bc}natives-1.12;' if self.version.startswith('1.12') else f'..{bc}natives;'
+
+            asset_path = f'.{bc}assets' if self.internal and os.path.isdir('assets') else f'..{bc}assets'
+            
+            java_command = [
+                f"..\\jre-21.0.2\\bin\\java{'w' if self.silent else ''}.exe",
+                "-Xverify:none",
+                f"-Xmx{settings.get('ram')}M",
+                f"-Djava.library.path={native_path}",
+                f"-cp {classpath}{path_sep}.{bc}{self.jar} {self.main_class}",
+                f"--username {settings.get('nickname')}",
+                "--gameDir .\\",
+                f"--assetsDir {asset_path}",
+                f"--assetIndex {self.version}",
+                "--uuid N/A",
+                "--accessToken 0",
+                "--userType legacy",
+                f"--version {self.version}"
+            ]
+
+            command = ' '.join(java_command)
             logger.debug(command)
 
-            # os.system(command)
-    
             process = Popen(command, stdout=PIPE, stderr=STDOUT)
             buffer = []
 
-            for l in process.stdout: 
-                print(l.decode(), end='')
-                buffer.append(l.decode())
+            for line in process.stdout:
+                _ = line.decode('utf-8', errors='ignore')
+                print(_, end='')
+                buffer.append(_)
 
             logchecker.checklogs(buffer)
 
