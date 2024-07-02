3ac0a74c2c78f9653cf8a4c555caab2fab3c8ad0
dest4590 <80628386+dest4590@users.noreply.github.com>
2024-07-02 10:10:25 +0300

    added updater downloading

--- collapse/utils/Updater.py
+++ collapse/utils/Updater.py
@@ -12,6 +12,8 @@ class Updater:
 
     def __init__(self) -> None:
         try:
+            self.latest_releases = self.get_latest_releases()
+            self.latest_release = self.latest_releases[0]
             self.remote_version = self.get_remote_version()
             self.latest_commit = self.get_latest_commit()
             self.local_version = data.version
@@ -36,9 +38,16 @@ class Updater:
             logger.error(f'Failed to fetch {path}: {e}')
             raise
 
+    def get_latest_releases(self) -> dict:
+        """Fetch releases from the GitHub API"""
+        return self.api_request('releases')
+
     def get_remote_version(self) -> str:
-        """Fetch the latest remote version from the GitHub API"""
-        return self.api_request('releases/latest').get('tag_name')
+        """Fetch the latest remote version from the GitHub API without considering pre-releases"""
+        latest_release = next((release for release in self.latest_releases if not release.get('prerelease')), None)
+        if latest_release:
+            return latest_release.get('tag_name')
+        return None
 
     def get_latest_commit(self) -> str:
         """Fetch the latest commit SHA from the GitHub API"""
@@ -49,8 +58,19 @@ class Updater:
         if self.remote_version and self.remote_version > self.local_version:
             logger.info('Update your loader!')
 
-            if selector.ask('Open a download page (y,n)'):
-                webbrowser.open(data.repo + 'releases/latest')
-
+            if selector.ask('Download a new version (y,n)'):
+                if self.latest_releases:
+                    if selector.ask('Dev version (y,n)'): # Prelease
+                        logger.debug('Downloading dev version')
+                        latest_prerelease = next((release for release in self.latest_releases if release.get('prerelease')), None)
+                        if latest_prerelease and latest_prerelease.get('assets'):
+                            webbrowser.open(latest_prerelease['assets'][0].get('browser_download_url'))
+                    else: # Release
+                        logger.debug('Downloading stable release')
+                        latest_release = next((release for release in self.latest_releases if not release.get('prerelease')), None)
+                        if latest_release and latest_release.get('assets'):
+                            webbrowser.open(latest_release['assets'][0].get('browser_download_url'))
+                else:
+                    logger.warn('No releases found')
 
 updater = Updater()
