237bef0b0b3bd25eb1f04a3ef6bc2ac9b0a0ce14
dest4590 <80628386+dest4590@users.noreply.github.com>
2024-07-04 12:30:30 +0300

    bump version to 1.3

--- collapse/static.py
+++ collapse/static.py
@@ -16,7 +16,7 @@ SHOW_HIDDEN_CHEATS = True
 # LOADER VARS
 REPO_URL = 'https://github.com/dest4590/CollapseLoader/'
 API_URL = 'https://web.collapseloader.org/'
-VERSION = '1.2.6'
+VERSION = '1.3'
 
 SERVERS = [
     'cdn.collapseloader.org',
