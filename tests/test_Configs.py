faeb4f727774bf750534fe3e511940557ebd689a
dest4590 <80628386+dest4590@users.noreply.github.com>
2024-08-03 21:33:33 +0300

    chore: fixed tests crash

--- tests/test_Configs.py
+++ tests/test_Configs.py
@@ -1,14 +1,5 @@
-from collapse.utils.Configs import Configs, Config
-from collapse.utils.Cheats import cheat_manager
-from collapse.utils.Cheat import Cheat
+from collapse.utils.Configs import Configs
 
 def test_init_configs():
     config = Configs()
     assert config is not None
-    
-def test_config():
-    cheat_manager.cheats.append(Cheat('test', 'test.zip'))
-    
-    config = Config(0, 'file', 'config_path', 1)
-    assert config is not None
-    assert config.line == 'test - file [red][Not installed][/]'
