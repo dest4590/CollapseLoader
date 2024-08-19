9f71ce8b978456112887257d9764187d3fb223ab
Purpl3 <80628386+dest4590@users.noreply.github.com>
2024-08-19 19:46:04 +0300

    Revert old settings logic

--- collapse/modules/storage/Settings.py
+++ collapse/modules/storage/Settings.py
@@ -12,7 +12,6 @@ class Settings(Module):
         self.file = file
         self.config = configparser.ConfigParser()
         self.config_path = data.get_local(self.file)
-        self._is_dirty = False  # Flag to track if config has been modified
 
         if os.path.exists(self.config_path):
             self.config.read(self.config_path)
@@ -22,29 +21,23 @@ class Settings(Module):
                 cfg.write('')
 
     def save(self) -> None:
-        """Save config to file only if it has been modified"""
-        if self._is_dirty:
-            with open(self.config_path, 'w', encoding='utf-8') as cfg:
-                self.config.write(cfg)
-            self._is_dirty = False
+        """Save config to file"""
+        with open(self.config_path, 'w', encoding='utf-8') as cfg:
+            self.config.write(cfg)
 
     def set(self, key: str, value: str, header: str = 'Options') -> None:
-        """Set a setting and mark config as modified"""
+        """Set a setting and save it to the config"""
         if header not in self.config:
             self.config[header] = {}
         self.config[header][key] = str(value)
-        self._is_dirty = True
+        self.save()
 
     def get(self, key: str, header: str = 'Options') -> str:
-        """Get a setting value from the cached config"""
+        """Get a setting value"""
         return self.config.get(header, key, fallback=False)
 
     def use_option(self, name: str) -> bool:
         """Check whether the setting is true or false, done for convenience"""
         return self.get(name) == 'False'
-
-    def __del__(self):
-        """Ensure config is saved when the object is destroyed"""
-        self.save()
 
 settings = Settings()
