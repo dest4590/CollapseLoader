31f75cdb75a6bf776c807782cfcc662a31c0c824
Purpl3 <80628386+dest4590@users.noreply.github.com>
2024-08-22 12:55:44 +0300

    Update analytics client request params

--- collapse/modules/network/Analytics.py
+++ collapse/modules/network/Analytics.py
@@ -2,6 +2,7 @@ from .Network import network
 from ..utils.Module import Module
 from ..storage.Data import data
 from ..network.API import api
+from ..storage.Settings import settings
 
 class Analytics(Module):
     """Class for handling analytics"""
@@ -15,6 +16,6 @@ class Analytics(Module):
         
     def client_run(self, client_id: int):
         """Send a request to the analytics server when the client runs"""
-        network.get(f'{api.server}api/analytics/client', params={'username': data.version, 'client_id': client_id})
+        network.get(f'{api.server}api/analytics/client', params={'username': settings.get('nickname'), 'client_id': client_id})
         
 analytics = Analytics()
