e9b546e8dec1c5047185f1db1eb887da8debe5ff
Purpl3 <80628386+dest4590@users.noreply.github.com>
2024-08-23 13:02:08 +0300

    Added logging for analytics module

--- collapse/modules/network/Analytics.py
+++ collapse/modules/network/Analytics.py
@@ -1,8 +1,9 @@
-from .Network import network
-from ..utils.Module import Module
-from ..storage.Data import data
 from ..network.API import api
+from ..storage.Data import data
 from ..storage.Settings import settings
+from ..utils.Module import Module
+from .Network import network
+
 
 class Analytics(Module):
     """Class for handling analytics"""
@@ -12,10 +13,12 @@ class Analytics(Module):
     
     def loader_start(self):
         """Send a request to the analytics server when the loader starts"""
+        self.debug('Send analytics request for loader start')
         network.get(f'{api.server}api/analytics/start', params={'version': data.version})
         
     def client_run(self, client_id: int):
         """Send a request to the analytics server when the client runs"""
+        self.debug('Send analytics request for client run')
         network.get(f'{api.server}api/analytics/client', params={'username': settings.get('nickname'), 'client_id': client_id})
-        
+
 analytics = Analytics()
