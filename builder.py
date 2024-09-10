5cad77fc94539959270013827ea9ff441313dd26
Purpl3 <dest4590@gmail.com>
2024-09-10 17:48:52 +0300

    removed unused code

--- builder.py
+++ builder.py
@@ -6,28 +6,30 @@ from glob import glob
 class Builder:
     """Class to build to .exe file"""
 
-    def __init__(self, onefile: bool = True, clean: bool = True, name: str = 'CollapseLoader', icon: str = 'logo.ico'):
-        self.onefile = onefile
-        self.clean = clean
+    def __init__(self, name: str = 'CollapseLoader', icon: str = 'logo.ico'):
         self.name = name
         self.icon = icon
 
     def build(self, removeBuild: bool = True):
         """Starts the build"""
+        
         # Remove old .exe builds
         for file in glob('*.exe'):
             os.remove(file)
         
         os.system(f'''pyinstaller --onefile --clean --console --name "{self.name}" --icon "collapse\\assets\\{self.icon}" run.py''')
 
+        # Move .exe file to root
         for file in glob('dist/*.exe'):
             shutil.move(file, './')
 
+        # Remove build folder
         if removeBuild:
             self.clear_all()
 
     def clear_all(self):
         """Deletes all unnecessary files"""
+        
         if os.path.exists('build/'):
             shutil.rmtree('build/')
 
@@ -39,4 +41,4 @@ class Builder:
             os.remove(spec)
 
 
-Builder().build()
+Builder(name='CollapseLoader_Dev_Build').build()
