3bf771e775cd396fe5c6a0e8a5ca20703a021a4b
Purpl3 <dest4590@gmail.com>
2024-09-10 18:22:24 +0300

    Added --disable-analytics argument for analytics

--- collapse/modules/network/Analytics.py
+++ collapse/modules/network/Analytics.py
@@ -1,3 +1,4 @@
+from ...arguments import args
 from ..network.API import api
 from ..storage.Data import data
 from ..storage.Settings import settings
@@ -10,9 +11,17 @@ class Analytics(Module):
     
     def __init__(self):
         super().__init__()
+        self.disabled = False
+        
+        if args.disable_analytics:
+            self.disabled = True
+            self.info('Analytics disabled')
     
     def loader_start(self):
         """Send a request to the analytics server when the loader starts"""
+        if self.disabled:
+            return
+        
         try:
             r = network.get(f'{api.server}api/analytics/start', params={'version': data.version}).json()
         
@@ -27,6 +36,9 @@ class Analytics(Module):
 
     def client_run(self, client_id: int):
         """Send a request to the analytics server when the client runs"""
+        if self.disabled:
+            return
+        
         try:
             r = network.get(f'{api.server}api/analytics/client', params={'username': settings.get('nickname'), 'client_id': client_id}).json()
         
