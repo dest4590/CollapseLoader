e77c23f7e18a74b54f5d41d94ffccb19185f8fa0
Purpl3 <dest4590@gmail.com>
2024-09-10 22:16:15 +0300

    Refactor argument parsing to handle unknown arguments

--- collapse/arguments.py
+++ collapse/arguments.py
@@ -4,4 +4,4 @@ parser = argparse.ArgumentParser(description='Your script description')
 parser.add_argument('-v', action='store_true', help='Enable debug logging')
 parser.add_argument('--disable-analytics', action='store_true', help='Disable analytics')
     
-args = parser.parse_args()
+args, unknown = parser.parse_known_args()
