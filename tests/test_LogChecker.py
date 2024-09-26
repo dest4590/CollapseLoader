840ff60e98e59551a90d2e2b8c8bedb1b70c9a08
Purpl3 <dest4590@gmail.com>
2024-09-26 08:28:10 +0300

    Fixed logchecker test

--- tests/test_LogChecker.py
+++ tests/test_LogChecker.py
@@ -1,6 +1,7 @@
 import pytest
 
 from collapse.modules.utils.LogChecker import LogChecker
+from collapse.modules.utils.Client import Client
 
 
 @pytest.fixture
@@ -8,9 +9,11 @@ def logchecker():
     logchecker = LogChecker()
     yield logchecker
 
-def test_LogChecker(logchecker: LogChecker):
-    assert logchecker.check_logs('Game crashed!')
-    assert logchecker.check_logs('Game crashed! java.lang.OutOfMemoryError: Java heap space')
-    
-def test_reason_LogChecker(logchecker: LogChecker):
-    assert logchecker.reason('it has too little heap memory') == 'Game crashed because it has too little heap memory'
+@pytest.fixture
+def client():
+    client = Client('Test', link='https://google.com')
+    yield client
+
+def test_LogChecker(logchecker: LogChecker, client: Client):
+    assert logchecker.check_logs('Game crashed!', client)
+    assert logchecker.check_logs('Game crashed! java.lang.OutOfMemoryError: Java heap space', client)
