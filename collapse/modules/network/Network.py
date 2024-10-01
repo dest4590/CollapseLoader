0a80cd330c70e50dc7d2e9713a82890652ec4ac3
dest4590 <dest4590@gmail.com>
2024-10-01 12:16:35 +0300

    Removed network request retries, fixed crash of sdkserver

--- collapse/modules/network/Network.py
+++ collapse/modules/network/Network.py
@@ -1,6 +1,4 @@
 import requests
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
 
 from ...arguments import args
 from ..utils.Module import Module
@@ -13,10 +11,6 @@ class Network(Module):
         super().__init__()
         
         self.session = requests.Session()
-        retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
-        adapter = HTTPAdapter(max_retries=retries)
-        self.session.mount('http://', adapter)
-        self.session.mount('https://', adapter)
         self.timeout = args.timeout if args.timeout else 1
 
     def get(self, url, params=None, headers=None, stream=False):
