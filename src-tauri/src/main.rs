ffe75066819aea7979b6db336d837d3b719d9ecf
dest4590 <dest4590@gmail.com>
2025-11-22 20:14:11 +0200

    feat: fixed build crash on linux skip-ci

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(target_os = "windows")]
 use windows::Win32::UI::HiDpi::{SetProcessDpiAwareness, PROCESS_SYSTEM_DPI_AWARE};
 
 fn main() {
